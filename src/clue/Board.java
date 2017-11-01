baee79ee1975b5cac23f772d6b2574548ab47ba8
Gabrielle Jones-154003549 <154003549@vusdnoc.vusd.k12.ca.us>
2017-11-01 10:16:43 -0700

    first commit

---
+++ src/clue/Board.java
@@ -0,0 +1,57 @@
+
+package clue;
+import java.awt.*;
+
+public class Board {
+    private final static int NUM_CONNECT_WIN = 4;    
+    
+    private final static int NUM_ROWS = 15;
+    private final static int NUM_COLUMNS = 15;      
+    private static Piece board[][] = new Piece[NUM_ROWS][NUM_COLUMNS];
+
+   
+    
+    public static void Reset() {
+ 
+        for (int zi = 0;zi<NUM_ROWS;zi++)
+        {
+            for (int zx = 0;zx<NUM_COLUMNS;zx++)
+            {
+                board[zi][zx] = null;
+            }
+        }
+        
+    }
+   
+
+    public static void Draw(Graphics2D g) {
+//Calculate the width and height of each board square.
+        int ydelta = Window.getHeight2()/NUM_ROWS;
+        int xdelta = Window.getWidth2()/NUM_COLUMNS;
+        
+ //draw grid
+        g.setColor(Color.black);
+        for (int zi = 1;zi<NUM_ROWS;zi++)
+        {
+            g.drawLine(Window.getX(0),Window.getY(zi*ydelta),
+                    Window.getX(Window.getWidth2()),Window.getY(zi*ydelta));
+        }
+        
+        for (int zi = 1;zi<NUM_COLUMNS;zi++)
+        {
+            g.drawLine(Window.getX(zi*xdelta),Window.getY(0),
+                    Window.getX(zi*xdelta),Window.getY(Window.getHeight2()));
+        }
+                
+        for (int zi = 0;zi<NUM_ROWS;zi++)
+        {
+            for (int zx = 0;zx<NUM_COLUMNS;zx++)
+            {
+                if (board[zi][zx] != null)
+                {
+                    board[zi][zx].draw(g,zi,zx,xdelta,ydelta);
+                }
+            }
+        } 
+    }
+}
